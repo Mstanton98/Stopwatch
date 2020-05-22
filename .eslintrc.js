8c2e3cc71a113932c067170501064bffb4d6d7cb
Mstanton98 <mstanton98@gmail.com>
2020-05-21 19:19:08 -0700

    setup scripts and ghpages stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,12 @@ module.exports = {
     parser: "@typescript-eslint/parser",
     parserOptions: {
         ecmaVersion: 2020,
-        sourceType: "module"
+        sourceType: "module",
     },
     extends: [
         "plugin:@typescript-eslint/recommended",
-        "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-        "plugin:prettier/recommended"
+        "prettier/@typescript-eslint",
+        "plugin:prettier/recommended",
     ],
     rules: {
         camelcase: 0,
